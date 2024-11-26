ec437e65582b68203dd348a1bb5ac327eeac78a3
Rohit Yadav <rohity5450@gmail.com>
2024-11-26 12:48:09 +0530

    Today contribt

--- src/index.ts
+++ src/index.ts
@@ -61,18 +61,34 @@ app.post("/api/v1/content", userMiddleware, async(req,res)=>{
     res.json({
         message: "Content added"
     })
-})
-
-app.get("/api/v1/content", (req,res)=>{
 
 })
 
-app.delete("/api/v1/content", (req,res)=>{
+app.get("/api/v1/content",userMiddleware,async(req,res)=>{
+    // @ts-ignore
+    const userId= req.userId;
+    const content = await ContentModel.find({
+        userId: userId
+    }).populate("userId", "username")
+    res.json({
+        content
+    })
+})
 
+app.delete("/api/v1/content", userMiddleware, async(req,res)=>{
+    const contentId = req.body.contentId;
+    await ContentModel.deleteMany({
+        contentId,
+        //@ts-ignore
+        userId: req.userId
+    })
+    res.json({
+        message:"deleted content"
+    })
 })
 
 app.post("api/v1/brain/share", (req,res)=>{
-
+    
 })
 
 app.get("api/v1/brain/:shareLink", (req,res)=>{
