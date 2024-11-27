635278c78758bc1e45d163e49f1c8ce657038bad
Rohit Yadav <rohity5450@gmail.com>
2024-11-27 13:48:52 +0530

    Updated the main

--- src/db.ts
+++ src/db.ts
@@ -7,8 +7,7 @@ mongoose.connect("mongodb+srv://hackoid:QqgXfS8g81Fh4EC0@cluster0.boz3z.mongodb.
 
 const UserSchema = new Schema({
     username: {type: String, unique: true},
-    password: String, 
-
+    password: String
 })
 
 export const UserModel = model("User", UserSchema);
@@ -17,11 +16,14 @@ const ContentSchema = new Schema({
     title: String,
     link: String,
     tags: [{type: mongoose.Types.ObjectId, ref: 'Tag'}],
-    userId: {type: mongoose.Types.ObjectId, ref: 'User', required: true}
-
+    type: String,
+    userId: {type: mongoose.Types.ObjectId, ref: 'User', required: true },
 })
 
+const LinkSchema = new Schema({
+    hash: String,
+    userId: {type: mongoose.Types.ObjectId, ref: 'User', required: true, unique: true },
+})
+
+export const LinkModel = model("Links", LinkSchema);
 export const ContentModel = model("Content", ContentSchema);
-
-
-
--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,66 @@
 import express from "express";
+import { random } from "./utlis";
 import jwt from "jsonwebtoken";
-import { ContentModel, UserModel } from "./db";
-
+import { ContentModel, LinkModel, UserModel } from "./db";
 import { JWT_PASSWORD } from "./config";
 import { userMiddleware } from "./middleware";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-app.post("/api/v1/signup",async (req,res)=> {
-    // zod validation
+app.post("/api/v1/signup", async (req, res) => {
+    // TODO: zod validation , hash the password
     const username = req.body.username;
     const password = req.body.password;
 
-    await UserModel.create({
-        username: username,
-        password: password
-    })
-
-    res.json({
-        message: "Users signed up"
-    })
-})
-
-app.post("/api/v1/signin", async(req,res)=>{
-
-    const username = req.body.username;
-    const password = req.body.password;
-    const existingUser = await UserModel.findOne({
-        username,
-        password
-    })
-
-    if(existingUser){
-        const token = jwt.sign({
-            id: existingUser._id 
-        }, JWT_PASSWORD)
+    try {
+        await UserModel.create({
+            username: username,
+            password: password
+        }) 
 
         res.json({
-            token 
+            message: "User signed up"
         })
-    } else {
-        res.status(403).json({
-            message: "Incorrect credentials"
+    } catch(e) {
+        res.status(411).json({
+            message: "User already exists"
         })
     }
 })
 
-app.post("/api/v1/content", userMiddleware, async(req,res)=>{
+app.post("/api/v1/signin", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const existingUser = await UserModel.findOne({
+        username,
+        password
+    })
+    if (existingUser) {
+        const token = jwt.sign({
+            id: existingUser._id
+        }, JWT_PASSWORD)
+
+        res.json({
+            token
+        })
+    } else {
+        res.status(403).json({
+            message: "Incorrrect credentials"
+        })
+    }
+})
+
+app.post("/api/v1/content", userMiddleware, async (req, res) => {
     const link = req.body.link;
     const type = req.body.type;
     await ContentModel.create({
         link,
         type,
-        //@ts-ignore
+        title: req.body.title,
         userId: req.userId,
         tags: []
     })
@@ -61,12 +68,12 @@ app.post("/api/v1/content", userMiddleware, async(req,res)=>{
     res.json({
         message: "Content added"
     })
-
+    
 })
 
-app.get("/api/v1/content",userMiddleware,async(req,res)=>{
+app.get("/api/v1/content", userMiddleware, async (req, res) => {
     // @ts-ignore
-    const userId= req.userId;
+    const userId = req.userId;
     const content = await ContentModel.find({
         userId: userId
     }).populate("userId", "username")
@@ -75,24 +82,87 @@ app.get("/api/v1/content",userMiddleware,async(req,res)=>{
     })
 })
 
-app.delete("/api/v1/content", userMiddleware, async(req,res)=>{
+app.delete("/api/v1/content", userMiddleware, async (req, res) => {
     const contentId = req.body.contentId;
+
     await ContentModel.deleteMany({
         contentId,
-        //@ts-ignore
         userId: req.userId
     })
+
     res.json({
-        message:"deleted content"
+        message: "Deleted"
     })
 })
 
-app.post("api/v1/brain/share", (req,res)=>{
-    
+app.post("/api/v1/brain/share", userMiddleware, async (req, res) => {
+    const share = req.body.share;
+    if (share) {
+            const existingLink = await LinkModel.findOne({
+                userId: req.userId
+            });
+
+            if (existingLink) {
+                res.json({
+                    hash: existingLink.hash
+                })
+                return;
+            }
+            const hash = random(10);
+            await LinkModel.create({
+                userId: req.userId,
+                hash: hash
+            })
+
+            res.json({
+                hash
+            })
+    } else {
+        await LinkModel.deleteOne({
+            userId: req.userId
+        });
+
+        res.json({
+            message: "Removed link"
+        })
+    }
 })
 
-app.get("api/v1/brain/:shareLink", (req,res)=>{
+app.get("/api/v1/brain/:shareLink", async (req, res) => {
+    const hash = req.params.shareLink;
+
+    const link = await LinkModel.findOne({
+        hash
+    });
+
+    if (!link) {
+        res.status(411).json({
+            message: "Sorry incorrect input"
+        })
+        return;
+    }
+    // userId
+    const content = await ContentModel.find({
+        userId: link.userId
+    })
+
+    console.log(link);
+    const user = await UserModel.findOne({
+        _id: link.userId
+    })
+
+    if (!user) {
+        res.status(411).json({
+            message: "user not found, error should ideally not happen"
+        })
+        return;
+    }
+
+    res.json({
+        username: user.username,
+        content: content
+    })
 
 })
 
-app.listen(3000);
+app.listen(3001);
